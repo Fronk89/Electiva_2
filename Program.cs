ea39323684ad2b2eedf77b11bdde8f3ea5597827
Frank Lafontaine <franklafontaine89@gmail.com>
2025-01-14 17:14:35 -0600

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Bienvenido al programa de calculo de area de triagunlos");
+    Console.ReadKey();
+    Console.WriteLine("Introdusca la altura porfavor");
+    int altura = int.Parse(Console.ReadLine());
+    Console.WriteLine("Introdusca la Base del triangulo porfavor");
+    int Base = int.Parse(Console.ReadLine());
+    int resultado = Base * altura / 2;
+    Console.WriteLine("El resultado es: " + resultado);
+    Console.ReadKey(); 
+}
